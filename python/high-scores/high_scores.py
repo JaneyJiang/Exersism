32d32850e97b871649fa5167f9e5eb36a98b772d
JaneyJiang <ljiang2008@126.com>
2019-01-14 13:44:25 +0800

    high-scores
    注意readme中出现的代码问题

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,20 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+        self.scores_sort = sorted(scores,reverse=True)
+
+    def latest(self):
+        return self.scores[-1]
+
+    def personal_best(self):
+        return self.scores_sort[0]
+
+    def personal_top(self):
+        return self.scores_sort[:3]
+        
+    def report(self):
+        latest_report = "Your latest score was {}. ".format(self.latest())
+        diff = self.personal_best()-self.latest()
+        personal_report = "That's {} short of your personal best!".format(diff) if diff else "That's your personal best!"
+        return latest_report+personal_report
+
