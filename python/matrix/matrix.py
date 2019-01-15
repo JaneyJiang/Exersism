e949c5313c2e3d90fe465bed04c88be695c29e69
JaneyJiang <ljiang2008@126.com>
2019-01-15 17:18:43 +0800

    string-->matrix
    解析字符串，通过换行和空格来提取数字矩阵

---
+++ python/matrix/matrix.py
@@ -0,0 +1,9 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.rows =  [[int(val) for val in row.split(' ')] for row in matrix_string.split('\n')]
+        self.columns = list(map(list,zip(*self.rows)))
+    def row(self, index):
+        return self.rows[index-1]
+
+    def column(self, index):
+        return self.columns[index-1]
