83c8ee5c3a5f7de2acc919d931406ac1740eb22f
JaneyJiang <ljiang2008@126.com>
2019-01-28 10:47:53 +0800

    Update acronym.py
    add a new method

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -3,3 +3,10 @@ def abbreviate(words):
     wordlist = re.split('[ -]',words)
     abv = [word[0].upper() for word in wordlist if word and word[0].isalpha()]
     return "".join(abv)
+
+'''
+#这个是找到第一个单词字母和有间隔隔开的第一个单词字母,的方式
+def abbreviate(string):
+    firstletters = re.findall("^[a-zA-Z]|[\s\-_][a-zA-Z]", string)
+    return ''.join([x[-1].upper() for x in firstletters])
+'''
